d2242ea04d91bcb4954053c983a45f36a4488c2d
Gabe <gungier1@gmail.com>
2024-05-29 16:28:42 -0400

    fixed background

--- src/utils/navToggle.ts
+++ src/utils/navToggle.ts
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.getElementById('hamburger');
-  const navbar = document.getElementById('navbar');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const hamburger = document.getElementById('hamburger');
+//   const navbar = document.getElementById('navbar');
 
-  hamburger.addEventListener('click', () => {
-    navbar.classList.toggle('open');
-  });
-});
+//   hamburger.addEventListener('click', () => {
+//     navbar.classList.toggle('open');
+//   });
+// });
